5369a4b0a03be56954da74471b4845c1ae489d2c
龔彥賓 <Ben@gongyanbinde-MacBook-Pro.local>
2017-05-28 20:23:55 +0800

    多層表單陣列和動態新增輸入框

--- src/app/modelform/modelform.component.ts
+++ src/app/modelform/modelform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from "@angular/forms";
+import { FormGroup, Validators, FormBuilder, FormArray } from "@angular/forms";
 @Component({
   selector: 'app-modelform',
   templateUrl: './modelform.component.html',
@@ -7,19 +7,27 @@ import { FormGroup, Validators, FormBuilder } from "@angular/forms";
 })
 export class ModelformComponent implements OnInit {
 
-  constructor(private fb:FormBuilder) { }
-  form:FormGroup
+  constructor(private fb: FormBuilder) { }
+  form: FormGroup
   ngOnInit() {
- this.form = this.fb.group({
-     title: 'This is title',
-     name: this.fb.group({
-       firstName: 'Will',
-       lastName: 'Huang'
-}) });
+    this.form = this.fb.group({
+      title: 'This is title',
+      name: this.fb.array([
+        this.fb.control('Will 1', Validators.required),
+        this.fb.control('Will 2', Validators.required)])
+    });
   }
-  submit(){
+  submit() {
 
   }
-
+  Add(){
+    const control = <FormArray> this.form.controls['name'];
+        control.push(this.fb.control('', Validators.required));
+  }
+Delete(i){
+  console.log("fdfs")
+   const control = <FormArray> this.form.controls['name'];
+        control.removeAt(i)
+}
 
 }
