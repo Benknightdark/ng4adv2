27cc2db9448698598e2aaf1a4002a73c8beb72da
Kung Ben <Kung Ben>
2017-05-31 17:01:14 +0800

    get viewchildcontent

--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BlockComponent implements OnInit {
 
   constructor() { }
-
+title="fdsa";
   ngOnInit() {
   }
 
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BlockComponent } from "app/block/block.component";
 
 @Component({
   selector: 'app-cards',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+ @ViewChild('block1')
+  block1: BlockComponent;
 
+  @ViewChild(BlockComponent)
+  block2: BlockComponent;
   constructor() { }
 
   ngOnInit() {
+  }
+  ngAfterViewInit() {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+
+    this.block1.title = 'Oh!';
+
   }
 
 }
