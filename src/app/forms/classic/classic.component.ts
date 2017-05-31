ca683f6600d490f359c68714a2f82025cf9b9f54
Kung Ben <Kung Ben>
2017-05-31 11:01:33 +0800

    json server

--- src/app/forms/classic/classic.component.ts
+++ src/app/forms/classic/classic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-classic',
@@ -6,13 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classic.component.css']
 })
 export class ClassicComponent implements OnInit {
-data:any={}
-  constructor() { }
+  data: any = {}
+  JSONoptions = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json;charset=utf-8' }) });
+
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
-  onSubmit(f){
-    console.log(f)
-  }
+  onSubmit(f) {
+    this.http.post
+      ("http://localhost:3000/forms", JSON.stringify(f.value), this.JSONoptions)
+      .subscribe(res => {
 
+        this.http.get
+          ("http://localhost:3000/forms").subscribe(res => console.log(res.json()))
+      }
+      )
+  }
 }
