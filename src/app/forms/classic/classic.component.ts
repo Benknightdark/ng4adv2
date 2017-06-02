15c7df57db4910c12a69631ff24a5fa6e7091c6b
龔彥賓 <Ben@gongyanbindeMBP.mshome.net>
2017-06-02 11:12:11 +0800

    template form checkbox

--- src/app/forms/classic/classic.component.ts
+++ src/app/forms/classic/classic.component.ts
@@ -21,10 +21,14 @@ export class ClassicComponent implements OnInit {
       console.log(d[0])
         this.data=d[0];
         this.showform=true;
+        console.log(this.data.habbits[0][1])
     })
   }
   AddNew() {
     this.data.list.push( this.data.list.length + 1)
+  }
+  ReturnObjectKeys(){
+   return Object.keys(this.data.habbits[0])
   }
   onSubmit(f) {
     // if (f.value.group1) {
